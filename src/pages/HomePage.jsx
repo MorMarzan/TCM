29c1a45ad639e9973674cbdced989002e68d8e4a
Mor Marzan <mormarzan@gmail.com>
2024-06-09 15:49:12 +0300

    Add content and vbasic style to hp

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,33 @@
+import logo from '/images/favicon.png'
 
 export function HomePage() {
     return (
-        <div className="home-page">
-            home page
+        <div className="home-page grid">
+            <section className="hero grid">
+                <div className='headers'>
+                    <h2>Taking Care of Myself</h2>
+                    <h4>Simple Tools for Self-Therapy</h4>
+                </div>
+                <div className="img-wrapper">
+                    <img src={logo}></img>
+                </div>
+            </section>
+            <section className="main">
+                <div className="img-wrapper">
+
+                </div>
+                <div className='content'>
+                    <p>TCM is a 3 month one-on-one program, where you will learn how to significantly improve the way you take care of yourself.</p>
+                    <p>It seems quite basic, and it’s actually not that hard, but surprisingly almost no one is good at it.</p>
+                    <p>The first 6 sessions cover the learning process and the rest will work towards supporting you to get established in taking really good care of yourself.</p>
+                    <p>The tools used as part of this program work, but the emphasis is not on any specific tool, it goes much deeper. The process helps develop clarity about what is most important for you and the determination to make your life much better.
+                        Through the program, you will learn how to break habit patterns which work against you, and instead develop habits that really help you. It will also strengthen your ability to observe your life and the choices you make through a new lens.</p>
+                    <p>It is simple, and quite easy, but it will only work if you’re willing to put in the work.</p>
+                    <p>Maybe it’s time for you to take better care of yourself?
+                        I’ll be happy to help.</p>
+                </div>
+            </section>
+            <section className="contact-us"></section>
         </div>
     )
 }

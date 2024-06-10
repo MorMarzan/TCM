a2dcad3a14e7dc3442bcf832f11cbe74f6aa61fd
Mor Marzan <mormarzan@gmail.com>
2024-06-10 18:46:44 +0300

    Mark content in hp

--- src/cmps/ContactUs.jsx
+++ src/cmps/ContactUs.jsx
@@ -14,6 +14,7 @@ export function ContactUs() {
     function handleSubmit(ev) {
         ev.preventDefault()
         console.log(userInfo)
+        //thank you and reset
     }
 
     const theme = createTheme({

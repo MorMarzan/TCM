2ae4a80b07012b5e0822534c78922b424b441058
Mor Marzan <mormarzan@gmail.com>
2024-07-09 15:34:32 +0300

    Fix netlify function trial

--- netlify/functions/hello-world/send-email.js
+++ netlify/functions/hello-world/send-email.js
@@ -1,31 +1,47 @@
-const mailgun = require('mailgun-js');
+// const mailgun = require('mailgun-js');
 
-exports.handler = async function (event, context) {
+// exports.handler = async function (event, context) {
+//   try {
+//     const { email, fullname, msg, phone } = JSON.parse(event.body);
+
+//     const DOMAIN = process.env.MAILGUN_DOMAIN;
+//     const apiKey = process.env.MAILGUN_API_KEY;
+//     const mg = mailgun({ apiKey: apiKey, domain: DOMAIN });
+
+//     const data = {
+//       from: email,
+//       to: 'Your Name <mormarzan@gmail.com>',
+//       subject: 'New Lead from TCM website',
+//       text: `${fullname} wrote: ${msg} contact details: ${email}, ${phone}`
+//     };
+
+//     const body = await mg.messages().send(data);
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(body)
+//     };
+//   } catch (error) {
+//     console.error('Error in sendEmail function:', error);
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Failed to send email', details: error.message })
+//     };
+//   }
+// };
+const handler = async (event) => {
   try {
-    const { email, fullname, msg, phone } = JSON.parse(event.body);
-
-    const DOMAIN = process.env.MAILGUN_DOMAIN;
-    const apiKey = process.env.MAILGUN_API_KEY;
-    const mg = mailgun({ apiKey: apiKey, domain: DOMAIN });
-
-    const data = {
-      from: email,
-      to: 'Your Name <mormarzan@gmail.com>',
-      subject: 'New Lead from TCM website',
-      text: `${fullname} wrote: ${msg} contact details: ${email}, ${phone}`
-    };
-
-    const body = await mg.messages().send(data);
-
+    const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify(body)
-    };
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
   } catch (error) {
-    console.error('Error in sendEmail function:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to send email', details: error.message })
-    };
+    return { statusCode: 500, body: error.toString() }
   }
-};
+}
+
+module.exports = { handler }

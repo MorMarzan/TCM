f047d413a7e6d2166faf50355a25fe60a3a52821
Mor Marzan <mormarzan@gmail.com>
2024-07-09 15:50:20 +0300

    Test send email func

--- netlify/functions/send-email/send-email.js
+++ netlify/functions/send-email/send-email.js
@@ -1,40 +1,40 @@
-// const mailgun = require('mailgun-js');
+const mailgun = require('mailgun-js');
 
-// exports.handler = async function (event, context) {
-//   try {
-//     const { email, fullname, msg, phone } = JSON.parse(event.body);
-
-//     const DOMAIN = process.env.MAILGUN_DOMAIN;
-//     const apiKey = process.env.MAILGUN_API_KEY;
-//     const mg = mailgun({ apiKey: apiKey, domain: DOMAIN });
-
-//     const data = {
-//       from: email,
-//       to: 'Your Name <mormarzan@gmail.com>',
-//       subject: 'New Lead from TCM website',
-//       text: `${fullname} wrote: ${msg} contact details: ${email}, ${phone}`
-//     };
-
-//     const body = await mg.messages().send(data);
-
-//     return {
-//       statusCode: 200,
-//       body: JSON.stringify(body)
-//     };
-//   } catch (error) {
-//     console.error('Error in sendEmail function:', error);
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: 'Failed to send email', details: error.message })
-//     };
-//   }
-// };
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello, this is the updated sendEmail function!" }),
-  };
+  try {
+    const { email, fullname, msg, phone } = JSON.parse(event.body);
+
+    const DOMAIN = process.env.MAILGUN_DOMAIN;
+    const apiKey = process.env.MAILGUN_API_KEY;
+    const mg = mailgun({ apiKey: apiKey, domain: DOMAIN });
+
+    const data = {
+      from: email,
+      to: 'Your Name <mormarzan@gmail.com>',
+      subject: 'New Lead from TCM website',
+      text: `${fullname} wrote: ${msg} contact details: ${email}, ${phone}`
+    };
+
+    const body = await mg.messages().send(data);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    };
+  } catch (error) {
+    console.error('Error in sendEmail function:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to send email', details: error.message })
+    };
+  }
 };
+// exports.handler = async function (event, context) {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: "Hello, this is the updated sendEmail function!" }),
+//   };
+// };
 
 // const handler = async (event) => {
 //   try {
